b3179c918aa9fccbb1b7b42fe6154f2bff4711c8
Alex Corona <aacoroona@gmail.com>
2023-02-06 22:06:04 -0600

    Puerto asignado

--- papeleria-corona/src/test/java/com/papelerias/papeleriacorona/ControladorHM.java
+++ papeleria-corona/src/test/java/com/papelerias/papeleriacorona/ControladorHM.java
@@ -1,2 +1,14 @@
-package com.papelerias.papeleriacorona;public class ControladorHM {
+package com.papelerias.papeleriacorona;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/saludar")
+public class ControladorHM {
+    @GetMapping("/hola")
+    public String saludar(){
+        return  "Prueba de controlador";
+    }
 }
