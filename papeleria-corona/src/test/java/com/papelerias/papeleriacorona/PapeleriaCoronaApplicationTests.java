327f3d5597e02f9f1fabaa940d790bdfbdeb3b87
Alex Corona <aacoroona@gmail.com>
2023-02-06 23:29:52 -0600

    Fin del proyecto

--- papeleria-corona/src/test/java/com/papelerias/papeleriacorona/PapeleriaCoronaApplicationTests.java
+++ papeleria-corona/src/test/java/com/papelerias/papeleriacorona/PapeleriaCoronaApplicationTests.java
@@ -1,11 +1,14 @@
 package com.papelerias.papeleriacorona;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PapeleriaCoronaApplicationTests {
-
+public class PapeleriaCoronaApplicationTests {
+	public static void main(String[] args) {
+		SpringApplication.run(PapeleriaCoronaApplicationTests.class, args);
+	}
 	@Test
 	void contextLoads() {
 	}
